e8dacb4e748543f5c1f0127aa22035d36e591f25
ZHANGW31 <50000741+ZHANGW31@users.noreply.github.com>
2020-01-23 11:22:25 -0800

    Added Features, Bug Fixes, Different Solution
    Changes:
    
    Added "How to" instructions for the user.
    Implemented Try-Catch-Finally for exception handling for the CalculateGrades method.
    Changed the flow of the application from taking user input to get the count of scores to having users enter as many scores as they want without specifically stating how many they want to enter.
    Fixed Bug where the calculations are incorrect if the user entered a value that is negative.

---
+++ SmartScoring.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace CalculatingAverages
+{
+    class Program
+    {
+        public static void CalculateGrades(int countOfInputsMax)
+        {
+            Console.WriteLine("\nThis console application will calculate your average grade given the scores you input!");
+            Console.WriteLine("\nTo begin, enter your scores! Make sure the values you enter are between 0 and 100!");
+            Console.WriteLine("\nWhen you are ready to calculate your average, enter 000 into the console to calculate your average!");
+            double sum = 0;
+            
+            char letterGrade = 'F';
+
+            //Try, Catch and Finally for exception handling in the for iteration and if statements.
+            try
+            {
+                //This for iteration takes the user input and evaluates for validity (numbers between 0-100)
+                for (int i = 1; i <= countOfInputsMax; i++)
+                {
+                    Console.Write($"\nPlease enter your next value: ");
+                    var strsum = Console.ReadLine();
+                    double userInput = double.Parse(strsum);
+
+                    //This If statement checks to see if the user entered 000, if they did, it evaluates their inputted values and breaks out of the loop.
+                    
+
+                    if (userInput == 000)
+                    {
+                        i--;
+                        double nowAverage = sum / i;
+                        if (nowAverage >= 90) letterGrade = 'A';
+                        if (nowAverage < 90 && nowAverage >= 80) letterGrade = 'B';
+                        if (nowAverage < 80 && nowAverage >= 70) letterGrade = 'C';
+                        if (nowAverage < 70 && nowAverage >= 60) letterGrade = 'D';
+                        if (nowAverage <= 59) letterGrade = 'F';
+
+                        Console.WriteLine($"\nThe total number of valid scores you entered is {i} and the average of the scores is {nowAverage} which is a {letterGrade}");
+                        Console.WriteLine("\nThank you for using this application!");
+                        break;
+                    }
+                    if (userInput > 100.0)
+                    {
+                        Console.WriteLine("Please enter a valid number between 0-100.");
+                        i--;
+                        sum = sum - userInput;
+
+                    }
+                    if (userInput < 0.0)
+                    {
+                        Console.WriteLine("Please enter a valid number between 0-100.");
+                        i--;
+                        userInput = 0;
+
+                    }
+                    else
+                    {
+                        sum = double.Parse(strsum) + sum;
+
+                    }
+
+                }
+                double averageOfSum = sum / countOfInputsMax;
+            }
+            catch
+            {
+                Console.WriteLine("Your entry was invalid!");
+
+            } finally
+            {
+                Console.WriteLine("Exiting App!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome!");
+            CalculateGrades(int.MaxValue);
+
+
+
+
+        }
+    }
+}
