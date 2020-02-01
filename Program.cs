248de7fa64afe941965c6360bd0d206964b28bd6
Wei Zhang <wei.zhang4@outlook.com>
2020-02-01 14:24:10 -0800

    Modified comments, cleaned up the code a little.

--- Program.cs
+++ Program.cs
@@ -10,26 +10,21 @@ namespace CalculatingAverages
             double sum = 0;
             countOfInputsMax = int.Parse(Console.ReadLine());
             char letterGrade = 'F';
-            
-
-            //This for iteration takes the user input and validates, and 
+            //This for iteration takes the user input and validates it for correctness (0-100) 
             for (int i = 1; i <= countOfInputsMax; i++)
             {
                 Console.Write($"\nPlease enter a value for number {i}: ");
                 var strsum = Console.ReadLine();
                 double userInput = double.Parse(strsum);
-
                 if(userInput > 100.0)
                 {
                     Console.WriteLine("Please enter a valid number between 0-100.");
-                    i--;
-                    
+                    i--;                  
                 }
                 if(userInput < 0.0)
                 {
                     Console.WriteLine("Please enter a valid number between 0-100.");
-                    i--;
-                    
+                    i--;                
                 }
                 else
                 {
@@ -38,29 +33,17 @@ namespace CalculatingAverages
 
             }
             double averageOfSum = sum / countOfInputsMax;
-
-
             if (averageOfSum >=90) letterGrade = 'A';
             if (averageOfSum < 90 && averageOfSum >= 80) letterGrade = 'B';
             if (averageOfSum < 80 && averageOfSum >= 70 ) letterGrade = 'C';
             if (averageOfSum < 70 && averageOfSum >= 60) letterGrade = 'D';
             if (averageOfSum <= 59) letterGrade = 'F';
-
-
             Console.WriteLine($"\nThe total number of scores you entered is {countOfInputsMax} and the average of the scores is {averageOfSum} which is a {letterGrade}");
         }
-         
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome!");
             Program.CalculateGrades(0);
-
-
-            //Console.WriteLine("To Calculate the average for your past 10 grades, please enter your scores from 0-100");
-            //Console.WriteLine("Enter your first number!");
-            //Validation.CalculateAverage(10);
-
         }
     }
 }
